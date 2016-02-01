b8a5940cdd2e4fb497f05f5b3bad8a480f095268
lilinhan <lilinhan1303@gmail.com>
2016-02-01 15:21:47 +0800

    noncopyable

---
+++ noncopyable.h
@@ -0,0 +1,29 @@
+/*************************************************************************
+    > File Name: noncopyable.h
+    > Author: lewin
+    > Mail: lilinhan1303@gmail.com
+    > Organization: Xiyou Linux Group
+    > Created Time: 2016年02月01日 星期一 15时10分43秒
+ ************************************************************************/
+
+#ifndef NETWORKLIB_NONCOPYABLE_H
+#define NETWORKLIB_NONCOPYABLE_H
+
+/*
+ *别的类继承noncopyable类来实现禁止复制的功能
+ */
+
+
+namespace NetworkLib{
+    class noncopyable{
+        protected:
+            //默认的析构函数和构造函数是保护的
+            noncopyable(){}
+            ~noncopyable(){}
+
+        private:
+            //私有化复制构造函数和复制赋值操作符
+            noncopyable(const noncopyable&);
+            const noncopyable& operator=(const noncopyable &);
+    }
+};
