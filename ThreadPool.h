9d9cee0186153ded3885273687c91c2fd73bbc8a
lilinhan <lilinhan1303@gmail.com>
2016-02-02 12:02:44 +0800

    thread pool

---
+++ ThreadPool.h
@@ -0,0 +1,46 @@
+/*************************************************************************
+    > File Name: ThreadPool.h
+    > Author: lewin
+    > Mail: lilinhan1303@gmail.com
+    > Organization: Xiyou Linux Group
+    > Created Time: 2016年02月01日 星期一 15时46分30秒
+ ************************************************************************/
+#pragma once
+
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<string>
+#include<mutex>
+#include<thread>
+#include<condition_variable>
+
+#include<assert.h>
+
+#include"noncopyable.h"
+
+namespace NetworkLib{
+    class ThreadPool:private noncopyable
+    {
+        private:
+            bool stop_;  //1:stop 0:run
+            std::vector<std::thread>  Works_;   //threads
+            std::queue<std::function<void()>> Tasks_;  //The task queue
+            //synchronization
+            std::mutex TasksMutex_; //Mutex for task queue
+            std::condition_variable Condition_;
+
+            void invoke(); //Threads invoke Functions
+        public:
+            ThreadPool(int ThreadsNum); //init the ThreadPool
+            ~ThreadPool();
+
+            //put function in task queue
+            void Enqueue(std::function<void ()> f);
+
+            //close the thread pool
+            void ShutDown();
+    };
+};
+
+
--- noncopyable.h
+++ noncopyable.h
@@ -5,10 +5,6 @@
     > Organization: Xiyou Linux Group
     > Created Time: 2016年02月01日 星期一 15时10分43秒
  ************************************************************************/
-
-#ifndef NETWORKLIB_NONCOPYABLE_H
-#define NETWORKLIB_NONCOPYABLE_H
-
 /*
  *别的类继承noncopyable类来实现禁止复制的功能
  */
@@ -24,5 +20,5 @@ namespace NetworkLib{
             //delete禁止编译器自动产生
             noncopyable(const noncopyable&) = delete;
             const noncopyable& operator=(const noncopyable &) = delete;
-    }
+    };
 };
